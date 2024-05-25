679d1cf9960965ecfd13d319b4524da496bfc48d
Barbara Rodriguez <barbs2089@gmail.com>
2024-05-25 19:25:01 +1000

    Add jest config files, setup scripts and add index.js file

---
+++ server/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+/**
+ * Home page
+ */
+ app.get("/", (req, res) => {
+  res.send("Welcome to the Raisely API");
+});
+
+app.listen(PORT, () => {
+	console.log(`Raisely example app listening at http://localhost:${PORT}`);
+});
+
+module.exports = app;
