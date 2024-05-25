b799d3febdd73ca958d4104f35a0279c0251cf4a
Barbara Rodriguez <barbs2089@gmail.com>
2024-05-25 19:41:50 +1000

    Add library folder for utils, add seeded donations file

---
+++ tests/seed/seedTestDonations.js
@@ -0,0 +1,11 @@
+let donations = [
+  {
+    id: "f7939023-3016-4a29-bffd-913f41b98598",
+    donorName: "Jane Smith",
+    amount: 5000,
+    profileId: "2ad19172-9683-407d-9732-8397d58ddcb2",
+    currency: "AUD"
+  }
+];
+
+module.exports = { donations };
