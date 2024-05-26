a8342f6cc60ac1568cbde59e2e7c25e4d0a0ece5
Barbara Rodriguez <barbs2089@gmail.com>
2024-05-26 22:43:52 +1000

    Add matching campaign profile helper to update campaign profiles

--- tests/seed/seedTestProfiles.js
+++ tests/seed/seedTestProfiles.js
@@ -8,11 +8,25 @@ const profiles = [
 		currency: "AUD"
 	},
 	{
+		id: "bb7fcf22-2c1e-4c63-8110-a19234401460",
+		name: "Another Campaign Profile",
+		total: 0,
+		parentId: null,
+		currency: "USD"
+	},
+	{
 		id: "2ad19172-9683-407d-9732-8397d58ddcb2",
 		name: "Nick's Fundraising Profile",
 		total: 0,
 		parentId: "78afca18-8162-4ed5-9a7b-212b98c9ec87",
 		currency: "AUD"
+	},
+	{
+		id: "f1efabfb-1e5b-4aad-89b7-517abedc117e",
+		name: "Amy's Fundraising Profile",
+		total: 0,
+		parentId: "bb7fcf22-2c1e-4c63-8110-a19234401460",
+		currency: "USD"
 	}
 ];
 
